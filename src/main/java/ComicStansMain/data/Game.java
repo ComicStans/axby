67c41e9e76826dce8e3214e1dbce88a68c17e185
dbc-hou <david.collins.hou@gmail.com>
2022-05-09 15:53:07 -0500

    hopefully without app props

---
+++ src/main/java/ComicStansMain/data/Game.java
@@ -0,0 +1,37 @@
+package ComicStansMain.data;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "axby_games")
+
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "art")
+    private String art;
+
+    @Column(name = "companies")
+    private String companies;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "platforms")
+    private String platforms;
+
+    @Column(name = "release_date")
+    private Date releaseDate;
+}
