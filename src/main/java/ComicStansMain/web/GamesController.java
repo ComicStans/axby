8f87b34f1923bcb00eeec2a13a07dd0943e27de7
Wesley Bernard <wesley.bernard64@gmail.com>
2022-05-10 11:29:00 -0400

    Creating post mapping, we'll have to change this along with the game class in the near future, I imagine

--- src/main/java/ComicStansMain/web/GamesController.java
+++ src/main/java/ComicStansMain/web/GamesController.java
@@ -4,12 +4,11 @@ package ComicStansMain.web;
 import ComicStansMain.data.Game;
 import ComicStansMain.data.GamesRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Date;
 
 @CrossOrigin
 @AllArgsConstructor
@@ -21,5 +20,12 @@ public class GamesController {
     @GetMapping
     private Collection<Game> getAll(){
         return gamesRepository.findAll();
+    }
+
+    @PostMapping
+    private void createGame(@RequestBody Game game) {
+        Date date = new Date(1459987200L);
+        game.setReleaseDate(date);
+        gamesRepository.save(game);
     }
 }
