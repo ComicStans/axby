2985a210ff2701213df54490c3fe7f7a0212dbf9
Wesley Bernard <wesley.bernard64@gmail.com>
2022-05-10 11:57:57 -0400

    Adding custom methods to GamesRepository

--- src/main/java/ComicStansMain/data/GamesRepository.java
+++ src/main/java/ComicStansMain/data/GamesRepository.java
@@ -3,4 +3,6 @@ package ComicStansMain.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GamesRepository extends JpaRepository<Game, Long> {
+    Game findByName(String Name);
+    Game findByCompaniesContains(String companies);
 }
