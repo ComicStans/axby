8b1d2df4dbee3611922ef0458516dd1b8563cbec
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-05-11 11:10:39 -0500

    working on modal

--- src/main/resources/static/js/Views/Register.js
+++ src/main/resources/static/js/Views/Register.js
@@ -6,11 +6,14 @@ export default function Register(props) {
         <html>
             <head>
                 <meta charset="UTF-8"/>
+                <link rel="stylesheet" href="./Re">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
                 <title>Register</title>
             </head>
             <body>
                 <h1>Register</h1>
-        
+                        <button id="myBtn">Open Modal</button>
+
                 <form id="register-form">
                     <label for="username">Username</label>
                     <input id="username" name="username" type="text"/>
@@ -20,6 +23,45 @@ export default function Register(props) {
                     <input id="password" name="password" type="password"/>
                     <button id="register-btn" type="button">Register</button>
                 </form>
+              <!-- The Modal -->
+                    <div id="myModal" class="modal">
+                    
+                      <!-- Modal content -->
+                      <div class="modal-content">
+                        <span class="close">&times;</span>
+                        <p>Some text in the Modal..</p>
+                      </div>
+                    
+                    </div>  
+                
+                    <script>
+                        // Get the modal
+                        let modal = document.getElementById("myModal");
+                        
+                        // Get the button that opens the modal
+                        let btn = document.getElementById("myBtn");
+                        
+                        // Get the <span> element that closes the modal
+                        let span = document.getElementsByClassName("close")[0];
+                        
+                        // When the user clicks the button, open the modal 
+                        btn.onclick = function() {
+                          modal.style.display = "block";
+                        }
+                        
+                        // When the user clicks on <span> (x), close the modal
+                        span.onclick = function() {
+                          modal.style.display = "none";
+                        }
+                        
+                        // When the user clicks anywhere outside of the modal, close it
+                        window.onclick = function(event) {
+                          if (event.target == modal) {
+                            modal.style.display = "none";
+                          }
+                        }
+                    </script>
+                
             </body>
         </html>
 `;
