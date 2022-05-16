fc60ea38a341675742982e43a1f312e735d2b6ad
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-05-16 09:58:19 -0500

    added css styling

--- src/main/resources/static/js/Views/Login.js
+++ src/main/resources/static/js/Views/Login.js
@@ -6,7 +6,7 @@ export default function Login(props) {
     <title>Log In</title>
 </head>
 <body>
-<!--<h1>Log In</h1>-->
+<h1>Log In</h1>
 
 <!-- Button trigger modal -->
 <!--<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#login">-->
@@ -14,16 +14,16 @@ export default function Login(props) {
 <!--</button>-->
 
 <!-- Modal -->
-<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Login</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
+<!--<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">-->
+<!--  <div class="modal-dialog" role="document">-->
+<!--    <div class="modal-content">-->
+<!--      <div class="modal-header">-->
+<!--        <h5 class="modal-title" id="exampleModalLabel">Login</h5>-->
+<!--        <button type="button" class="close" data-dismiss="modal" aria-label="Close">-->
+<!--          <span aria-hidden="true">&times;</span>-->
+<!--        </button>-->
+<!--      </div>-->
+<!--      <div class="modal-body">-->
 <form id="login-form">
     <label for="username">Email</label>
     <input id="username" name="username" type="text"/><br>
@@ -31,14 +31,14 @@ export default function Login(props) {
     <input id="password" name="password" type="password"/>
     <input id="login-btn" type="submit" value="Log In"/>
 </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+<!--      </div>-->
+<!--      <div class="modal-footer">-->
+<!--        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>-->
+<!--        <button type="button" class="btn btn-primary">Save changes</button>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</div>-->
 
 
 
