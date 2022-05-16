7545c04344eb01b71159aaba5c15a6e8beb235c6
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-05-16 09:51:13 -0500

    adds profile changes

--- src/main/resources/static/js/Views/Navbar.js
+++ src/main/resources/static/js/Views/Navbar.js
@@ -9,42 +9,39 @@ export default function Navbar(props) {
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
+       <ul class="navbar-nav glow">
           <li class="nav-item">
-  <a href="/" data-link>Home</a>
-      </li>
-      <li class="nav-item active">
-            <a href="/marketPlace" data-link>Market Place</a>
-      </li>
-      <li class="nav-item">
-            <a href="/messageBoards" data-link>Message Boards</a>
-      </li>
-
-      <li class="nav-item">
-            <a href="/friends" data-link>Friends</a>
-      </li>
-            <li class="nav-item">
-            <a id="login-link" data-toggle="modal" data-target="#login" data-link href="#"" data-link>Login</a>
-      </li>
-      <li class="nav-item">
-            <a id="register-link" data-toggle="modal" data-target="#register" data-link href="#">Register</a>
-      </li>
-
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          More
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="/profile" data-link>Profile</a></a>
-          <a class="dropdown-item" href="/about" data-link>About</a>
-          <a class="dropdown-item" href="/account" data-link>Account</a>
-        </div>
-      </li>
-      <li><!-- Button trigger modal -->
+                <a href="/" data-link>Home</a>
+          </li>
+          <li class="nav-item active">
+                <a href="/marketPlace" data-link>Market Place</a>
+          </li>
+          <li class="nav-item">
+                <a href="/messageBoards" data-link>Message Boards</a>
+          </li>
+          <li class="nav-item">
+                <a href="/friends" data-link>Friends</a>
+          </li>
+          <li class="nav-item">
+                <a id="login-link" data-toggle="modal" data-target="#login" data-link href="#"" data-link>Login</a>
+          </li>
+          <li class="nav-item">
+                <a id="register-link" data-toggle="modal" data-target="#register" data-link href="#">Register</a>
+          </li>
+    
+          <li class="nav-item dropdown">
+             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">More</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="/profile" data-link>Profile</a></a>
+                  <a class="dropdown-item" href="/about" data-link>About</a>
+                  <a class="dropdown-item" href="/account" data-link>Account</a>
+                </div>
+          </li>
+<!--      <li>&lt;!&ndash; Button trigger modal &ndash;&gt;-->
 <!--<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">-->
 <!--  Register-->
 <!--</button></li>-->
-    </ul>
+       </ul>
   </div>
 </nav>
 ${Register(props)}
