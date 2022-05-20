9939a6199998377adbebd5523ea27b3aef010a68
dbc-hou <david.collins.hou@gmail.com>
2022-05-20 15:21:55 -0500

    GuildController created and tested

--- src/main/java/ComicStansMain/data/User.java
+++ src/main/java/ComicStansMain/data/User.java
@@ -85,8 +85,8 @@ public class User {
 
     @JoinTable(
             name="axby_guild_user",
-            joinColumns = {@JoinColumn(name = "id", nullable = false, updatable = false)},
-            inverseJoinColumns = {@JoinColumn(name="user_id", nullable = false, updatable = false)},
+            joinColumns = {@JoinColumn(name = "user_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name="guild_id", nullable = false, updatable = false)},
             foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT),
             inverseForeignKey = @ForeignKey(ConstraintMode.CONSTRAINT)
     )
--- src/main/java/ComicStansMain/web/GuildsController.java
+++ src/main/java/ComicStansMain/web/GuildsController.java
@@ -6,6 +6,7 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
 
 @CrossOrigin
@@ -38,21 +39,35 @@ public class GuildsController {
 
     @PostMapping
     private void createGuild(@RequestBody Guild newGuild) {
-        newGuild.setCreator(usersRepository.findByEmail("bob@bob.com"));
+        User theCreator = usersRepository.findByEmail("bob@bob.com");
+        Collection<Guild> theGuilds = theCreator.getGuilds();
+        newGuild.setCreator(theCreator);
         newGuild.setDateCreated(LocalDate.now());
+        theGuilds.add(newGuild);
+        theCreator.setGuilds(theGuilds);
         guildsRepository.save(newGuild);
+        usersRepository.save(theCreator);
     }
 
     @PutMapping("{id}")
-    private void modifyBoard(@PathVariable Long id, @RequestBody Guild guildToEdit) {
+    private void modifyGuild(@PathVariable Long id, @RequestBody Guild guildToEdit) {
         Guild thisGuild = guildsRepository.getById(id);
         thisGuild.setName(guildToEdit.getName());
         thisGuild.setMotto(guildToEdit.getMotto());
         guildsRepository.save(thisGuild);
     }
 
+    @PutMapping("newMember/{id}")
+    private void addGuildMember(@PathVariable Long id, @RequestBody Guild thisGuild) {
+        User userToAdd = usersRepository.getById(id);
+        Collection<Guild> myGuilds = userToAdd.getGuilds();
+        myGuilds.add(guildsRepository.getById(thisGuild.getId()));
+        userToAdd.setGuilds(myGuilds);
+        usersRepository.save(userToAdd);
+    }
+
     @DeleteMapping("{id}")
-    private void deleteBoard(@PathVariable Long id) {
+    private void deleteGuild(@PathVariable Long id) {
         guildsRepository.deleteById(id);
     }
 }
