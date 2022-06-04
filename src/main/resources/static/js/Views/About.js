0c899b54c96fc4ab3755ea10c6d691758e218e09
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-06-03 23:13:19 -0500

    editing about page

--- src/main/resources/static/js/Views/About.js
+++ src/main/resources/static/js/Views/About.js
@@ -4,18 +4,20 @@ export default function About(props) {
             <h1 class="aboutUs">About Us</h1>
         </header>
         <main>
-            <div >
-                <p id="about">To put it simple, this site was inspired by the love of playing video games and nostalgia. When COVID hit in 2020, everyone, quite literally, was looking for something to do at home. Purchasing any recent games became an expensive hobby, so a ton of people turned to retro games to curve the financial burden and to also feed that longing feeling for "the good 'ol days". With so many people getting the same idea, these limited game consoles and cartridges became even more limited! The hoarding, much like toilet paper, had begun! With these stacks of games in our houses and the many distractions of 2020 like tigers, murder hornets, and wildfires, it became harder to keep track of your game play, which games you already purchased, and which games you wanted to keep an eye out for! <br>With Gamerz Corner© , you can keep track of all of this along with connecting with friends, reading and posting in message boards to ask for strategies, cheat codes, if anyone has any memory of a game you played as a kid with your extremely vague description...even posting your own rating on your library of what YOU thought of the game.<br><br><h5 class="ourTeam">This site was developed by our team:</p>  
+         <p id="about">To put it simple, this site was inspired by the love of playing video games and nostalgia. When COVID hit in 2020, everyone, quite literally, was looking for something to do at home. Purchasing any recent games became an expensive hobby, so a ton of people turned to retro games to curve the financial burden and to also feed that longing feeling for "the good 'ol days". With so many people getting the same idea, these limited game consoles and cartridges became even more limited! The hoarding, much like toilet paper, had begun! With these stacks of games in our houses and the many distractions of 2020 like tigers, murder hornets, and wildfires, it became harder to keep track of your game play, which games you already purchased, and which games you wanted to keep an eye out for! <br>With Gamerz Corner© , you can keep track of all of this along with connecting with friends, reading and posting in message boards to ask for strategies, cheat codes, if anyone has any memory of a game you played as a kid with your extremely vague description...even posting your own rating on your library of what YOU thought of the game.<br><br><h5 class="ourTeam">This site was developed by our team:</p>  
+
+            <div id="alignOurTeam">
                 <h5 class="ourTeam">Wesley Bernard<br>
-                
+                                <img class="aboutPic" src="../../Images/Wesley.png" alt"Wesley"></img>
                 <h5 class="ourTeam">David Collins<br>
-                
+                                <img class="aboutPic" src="../../Images/DavidHeadshot.jpeg" alt"David"></img>
                 <h5 class="ourTeam">Jennifer Deleon<br>
-                
+                                <img class="aboutPic" src="../../Images/JenniferHeadshot.jpg" alt"Jennifer"></img>
                 <h5 class="ourTeam">Rachel Verastique<br>
-                
+                                <img class="aboutPic" src="../../Images/Rachel.png" alt"Rachel"></img>
                 <h5 class="ourTeam">Micah Wood<br>
-                
+                               <img class="aboutPic" src="../../Images/MicahHeadshot.jpeg" alt"Micah"></img>
+
             </div>
         </main>
     `;
