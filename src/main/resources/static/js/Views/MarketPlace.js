104b54cfba0df518fcce4a3971b2652be6cc185c
Rachel C Verastique <Rachel.C.Verastique@gmail.com>
2022-06-07 16:29:46 -0500

    Added hard coded ads to market place

--- src/main/resources/static/js/Views/MarketPlace.js
+++ src/main/resources/static/js/Views/MarketPlace.js
@@ -6,55 +6,155 @@ export default function MarketPlace(props) {
         
     </head>
       <body>
-        <h1 style="text-align: center">Market Place</h1>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-2">
-                <div class="dropdown">
-                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </button>
-                  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                    <button class="dropdown-item" type="button">Action</button>
-                    <button class="dropdown-item" type="button">Another action</button>
-                    <button class="dropdown-item" type="button">Something else here</button>
-                  </div>
+        <h1 style="text-align: center; font-size: 4em" id="marketPlace">Market Place</h1>
+<!--    <div class="container">-->
+<!--        <div class="row">-->
+<!--            <div class="col-sm-2">-->
+<!--                <div class="dropdown">-->
+<!--                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-expanded="false">-->
+<!--                    Dropdown-->
+<!--                  </button>-->
+<!--                  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">-->
+<!--                    <button class="dropdown-item" type="button">Action</button>-->
+<!--                    <button class="dropdown-item" type="button">Another action</button>-->
+<!--                    <button class="dropdown-item" type="button">Something else here</button>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        -->
+     
+        <div class="row d-flex justify-content-around">
+            <a class="game-image" href="#">
+               <div class="card" style="width: 14rem;">
+                  <img src="images/smcase.jpeg"  class="card-img-top" alt="super mario bros cartridge">
+                      <div class="card-body">
+                        <h5 class="card-title">Super Mario Bros 2</h5>
+                        <p class="card-text">Super Mario Brothers 2 for Nintendo. Good condition, comes with original case. Willing to ship within the USA if you take care of shipping. </p>
+                        <a href="#" class="btn btn-primary">Contact Seller</a>
+                      </div>
                 </div>
-            </div>
+            </a>            
+           <a class="game-image" href="#">
+               <div class="card" style="width: 14rem;">
+                  <img src="images/mb.jpeg"  class="card-img-top" alt="super mario bros cartridge">
+                      <div class="card-body">
+                        <h5 class="card-title">Super Mario World</h5>
+                        <p class="card-text">Super Mario world for the Super Nintendo Console. Excellent condition. Does not come with original case. 
+                         local pick up in San Antonio, Texas.</p>
+                        <a href="#" class="btn btn-primary">Contact Seller</a>
+                      </div>
+                </div>
+            </a> 
+            <a class="game-image" href="#">
+               <div class="card" style="width: 14rem;">
+                  <img src="images/360bop.jpeg"  class="card-img-top" alt="super mario bros cartridge">
+                      <div class="card-body">
+                        <h5 class="card-title">COD Black Ops 2</h5>
+                        <p class="card-text">For XBOX 360. Comes with original case but it does have a small crack on the backside. Disc has no scratches. Willing to ship or local pickup Scranton, Pa.</p>
+                        <a href="#" class="btn btn-primary">Contact Seller</a>
+                      </div>
+                </div>
+            </a> 
+            <a class="game-image" href="#">
+               <div class="card" style="width: 14rem;">
+                  <img src="images/racps.jpeg"  class="card-img-top" alt="super mario bros cartridge">
+                      <div class="card-body">
+                        <h5 class="card-title">Ratchet and Clank: Going Commando</h5>
+                        <p class="card-text">Good condition, no scratches, original case no damage. Shipping only!</p>
+                        <a href="#" class="btn btn-primary">Contact Seller</a>
+                      </div>
+                </div>
+            </a> 
         </div>
-        
-        <div class="card" style="width: 18rem;">
-          <img src="images/smb1.jpeg" class="card-img-top" alt="super mario bros cartridge">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
+
+        <div class="row d-flex justify-content-around">
+            <a class="game-image" href="#">
+               <div class="card" style="width: 14rem;">
+                  <img src="images/asegg.jpeg"  class="card-img-top" alt="super mario bros cartridge">
+                      <div class="card-body">
+                        <h5 class="card-title">Aladin</h5>
+                        <p class="card-text">Aladdin for Sega Genesis. No case, cartridge has a small chip on the bottom edge but still works perfectly.</p>
+                        <a href="#" class="btn btn-primary">Contact Seller</a>
+                      </div>
+                </div>
+            </a>            
+           <a class="game-image" href="#">
+               <div class="card" style="width: 14rem;">
+                  <img src="images/dh.jpeg"  class="card-img-top" alt="super mario bros cartridge">
+                      <div class="card-body">
+                        <h5 class="card-title">DUCK HUNT!</h5>
+                        <p class="card-text">You won't find this game in excellent condition like this! Origional case and it STILL SMELLS LIKE 1984! Great buy! Shipping only!</p>
+                        <a href="#" class="btn btn-primary">Contact Seller</a>
+                      </div>
+                </div>
+            </a> 
+            <a class="game-image" href="#">
+               <div class="card" style="width: 14rem;">
+                  <img src="images/mkb.jpeg"  class="card-img-top" alt="super mario bros cartridge">
+                      <div class="card-body">
+                        <h5 class="card-title">Mortal Kombat</h5>
+                        <p class="card-text">Its a good game, you should buy it. Good condition. Local pick up only Las Vegas, Nv.</p>
+                        <a href="#" class="btn btn-primary">Contact Seller</a>
+                      </div>
+                </div>
+            </a> 
+            <a class="game-image" href="#">
+               <div class="card" style="width: 14rem;">
+                  <img src="images/snc.jpeg"  class="card-img-top" alt="super mario bros cartridge">
+                      <div class="card-body">
+                        <h5 class="card-title">Sonic: Mega Collection</h5>
+                        <p class="card-text">Sonic for Nintendo Game Cube, Used only a month, great condition, no scratches and case is perfect. Shipping in the US only.</p>
+                        <a href="#" class="btn btn-primary">Contact Seller</a>
+                      </div>
+                </div>
+            </a> 
         </div>
-<!--        <div class="row d-flex justify-content-around">-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a> -->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--        </div>-->
-<!--          <div class="row d-flex justify-content-around">-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a> -->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--        </div>-->
-<!--          <div class="row d-flex justify-content-around">-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a> -->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--        </div>-->
-<!--          <div class="row d-flex justify-content-around">-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a> -->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--            <a class="game-image" href="#"><i class="fas fa-gamepad fa-8x"></i></a>-->
-<!--        </div>-->
+
+        <div class="row d-flex justify-content-around">
+            <a class="game-image" href="#">
+               <div class="card" style="width: 14rem;">
+                  <img src="images/n64.jpeg"  class="card-img-top" alt="super mario bros cartridge">
+                      <div class="card-body">
+                        <h5 class="card-title">Red Nintendo 64 console</h5>
+                        <p class="card-text">All original cords, great condition, everything works. Local pickup only Austin, Texas</p>
+                        <a href="#" class="btn btn-primary">Contact Seller</a>
+                      </div>
+                </div>
+            </a>            
+<!--           <a class="game-image" href="#">-->
+<!--               <div class="card" style="width: 14rem;">-->
+<!--                  <img src="images/mb.jpeg"  class="card-img-top" alt="super mario bros cartridge">-->
+<!--                      <div class="card-body">-->
+<!--                        <h5 class="card-title">Card title</h5>-->
+<!--                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+<!--                        <a href="#" class="btn btn-primary">Go somewhere</a>-->
+<!--                      </div>-->
+<!--                </div>-->
+<!--            </a> -->
+<!--            <a class="game-image" href="#">-->
+<!--               <div class="card" style="width: 14rem;">-->
+<!--                  <img src="images/mb.jpeg"  class="card-img-top" alt="super mario bros cartridge">-->
+<!--                      <div class="card-body">-->
+<!--                        <h5 class="card-title">Card title</h5>-->
+<!--                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+<!--                        <a href="#" class="btn btn-primary">Go somewhere</a>-->
+<!--                      </div>-->
+<!--                </div>-->
+<!--            </a> -->
+<!--            <a class="game-image" href="#">-->
+<!--               <div class="card" style="width: 14rem;">-->
+<!--                  <img src="images/mb.jpeg"  class="card-img-top" alt="super mario bros cartridge">-->
+<!--                      <div class="card-body">-->
+<!--                        <h5 class="card-title">Card title</h5>-->
+<!--                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+<!--                        <a href="#" class="btn btn-primary">Go somewhere</a>-->
+<!--                      </div>-->
+<!--                </div>-->
+<!--            </a> -->
+        </div>
+
+
         
         
         
